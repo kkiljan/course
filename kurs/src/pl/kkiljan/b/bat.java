bdbf68a8d764b9b5c0fd0b92d1a73042b9e91ce3
kkiljan <konrad.kiljan@gmail.com>
2018-04-28 17:10:17 +0200

    some comment

---
+++ kurs/src/pl/kkiljan/b/bat.java
@@ -0,0 +1,24 @@
+package pl.kkiljan.b;
+
+public class bat {
+    private float soundFrequency;
+    private float weight;
+
+    public bat() {
+    }
+
+    public bat(float soundFrequency, float weight) {
+        this.soundFrequency = soundFrequency;
+        this.weight = weight;
+
+    }
+
+    public float getSoundFrequency{
+        return soundFrequency;
+
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
