8c16557ae19093e4d386197c99724aa436e0c34e
kkiljan <konrad.kiljan@gmail.com>
2018-04-28 16:02:16 +0200

    Initial commit

---
+++ kurs/src/pl/kkiljan/Temperature.java
@@ -0,0 +1,26 @@
+package pl.kkiljan;
+
+public class Temperature
+    {
+    private double value;
+
+    public Temperature(double value)
+        {
+        this.value = value;
+        }
+
+
+    public void ifPositive()
+        {
+        if (this.value > 0)
+            {
+            System.out.println("Temperatura jest dodatnia");
+
+            } else
+            {
+            System.out.println("Temperatura jest ujemna lub r\u00f3wna zero");
+
+            }
+
+        }
+    }
---
+++ kurs/src/pl/kkiljan/Test.java
@@ -0,0 +1,13 @@
+package pl.kkiljan;
+
+
+public class Test
+    {
+    public static void main(String[] args)
+        {
+        Temperature temp = new Temperature(12);
+        temp.ifPositive();
+
+
+        }
+    }
