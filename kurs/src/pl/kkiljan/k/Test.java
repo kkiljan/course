819b0eefc05c519277b344192c1021d897d8f5ba
kkiljan <konrad.kiljan@gmail.com>
2018-04-30 10:10:00 +0200

    conversion and casting

---
+++ kurs/src/pl/kkiljan/k/Test.java
@@ -0,0 +1,9 @@
+package pl.kkiljan.k;
+
+public class Test {
+    public static void main(String[] args) {
+        long [] numbers = new long[10000];
+
+
+    }
+}
---
+++ kurs/src/pl/kkiljan/l/Test4.java
@@ -0,0 +1,10 @@
+package pl.kkiljan.l;
+
+public class Test4 {
+    public static void main(String[] args) {
+        char ch = ' ';
+        int intAscii = ch;
+
+        System.out.println(intAscii);
+    }
+}
