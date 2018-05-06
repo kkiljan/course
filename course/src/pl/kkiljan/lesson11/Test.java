c2c7120c7153d25b5f5dadcceb1c530672920dc9
kkiljan <konrad.kiljan@gmail.com>
2018-05-06 16:52:48 +0200

    Referencja, sterta i GC w Java

--- course/src/pl/kkiljan/lesson11/Test.java
+++ course/src/pl/kkiljan/lesson11/Test.java
@@ -1,8 +1,15 @@
 package pl.kkiljan.lesson11;
 
+/*
+Spróbuj napisać program, który spowoduje przepełnienie sterty i skończy się wyjątkiem java.lang.OutOfMemoryError.
+ */
+
+/*
+Program może nie przepełniać sterty na komputerze z większą ilością pamięci niż maszyna na której program był testowany
+ */
 public class Test {
     public static void main(String[] args) {
-        long [] numbers = new long[10000];
+        long [] numbers = new long[100000000];
 
 
     }
