446f4d91a508a6fd551aeca1bc3458278728f841
kkiljan <konrad.kiljan@gmail.com>
2018-05-06 16:29:23 +0200

    pierwszy program

--- course/src/pl/kkiljan/lesson03/test.java
+++ course/src/pl/kkiljan/lesson03/test.java
@@ -1,6 +1,10 @@
 package pl.kkiljan.lesson03;
 
-public class test {
+/*
+Napisz program, który wyświetli trzy różne zdania opisujące aktualną pogodę. W czwartej linii wypisz sumaryczną długość trzech poprzednich zdań.
+ */
+
+public class Test {
     public static void main(String[] args) {
         String message1 = "Dziś jest słoneczna pogoda.";
         String message2 = "Temperatura w okolicach 20 stopni.";
